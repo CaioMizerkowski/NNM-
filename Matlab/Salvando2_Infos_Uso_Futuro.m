8c8e207ff9463ceabd52ef161db7b3e6b041af61
CaioMizerkowski <caiomizerkowski@gmail.com>
2017-10-07 18:26:07 -0300

    SAVE
    Não vou mexer nessa merda

--- Matlab/Salvando2_Infos_Uso_Futuro.m
+++ Matlab/Salvando2_Infos_Uso_Futuro.m
@@ -1,6 +1,6 @@
 function [] = Salvando2_Infos_Uso_Futuro
     Data = load('data_LDMOS.mat','in_extraction','out_extraction');
-    M = 5;
+    M = 2;
     Dados_Saida = Data.out_extraction;
     Dados_Entrada = [real(Data.in_extraction),imag(Data.in_extraction),abs(Data.in_extraction),angle(Data.in_extraction)];
     %%%
